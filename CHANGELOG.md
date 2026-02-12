b83cc848dd3609abac2a78cd20965c69fdae92cb
RosApr <287134285@qq.com>
2026-02-12 10:19:20 +0800

    refactor: use tsdown instead of tsup

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## v0.1.2 / 2026-02-12
+
+- refactor: use tsdown instead of tsup
+- chore(deps): update deps
+
 ## v0.1.1 / 2025-10-26
 
 - fix(types): use HoaMiddleware
---
+++ tsdown.config.js
@@ -0,0 +1,38 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig([
+  // ESM build
+  {
+    entry: ['src/**/*.js'],
+    format: 'esm',
+    dts: false,
+    splitting: false,
+    sourcemap: false,
+    clean: true,
+    minify: false,
+    unbundle: true,
+    outDir: 'dist/esm',
+    target: 'es2020',
+    esbuildOptions (options) {
+      options.drop = ['console', 'debugger']
+      options.legalComments = 'none'
+    }
+  },
+  // CJS build
+  {
+    entry: ['src/**/*.js'],
+    format: 'cjs',
+    dts: false,
+    splitting: false,
+    sourcemap: false,
+    clean: true,
+    minify: false,
+    unbundle: true,
+    outDir: 'dist/cjs',
+    target: 'es2020',
+    esbuildOptions (options) {
+      options.drop = ['console', 'debugger']
+      options.legalComments = 'none'
+    }
+  }
+])
